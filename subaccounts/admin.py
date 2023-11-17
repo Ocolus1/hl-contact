91f5c1dbb8f374011750babb0f92ab0ad03c5a98
Paul <awolesiboluwatife@gmail.com>
2023-11-17 10:58:47 +0100

    added customUser

--- subaccounts/admin.py
+++ subaccounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SubAccount, Contact, Tag, PurchasedPhoneNumber, A2PRegistration
+from .models import SubAccount, Contact, Tag, PurchasedPhoneNumber, A2PRegistration, CustomUser
 
 # Register your models here.
 admin.site.register(SubAccount)
@@ -7,3 +7,4 @@ admin.site.register(Contact)
 admin.site.register(PurchasedPhoneNumber)
 admin.site.register(Tag)
 admin.site.register(A2PRegistration)
+admin.site.register(CustomUser)
