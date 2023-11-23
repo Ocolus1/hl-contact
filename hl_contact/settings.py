97612b2c5ec19817a4db53518b7b7f7e7beef7a0
Paul <awolesiboluwatife@gmail.com>
2023-11-23 11:43:22 +0100

    chhanged allowed hosts

--- hl_contact/settings.py
+++ hl_contact/settings.py
@@ -30,13 +30,11 @@ DB_USER = env("DB_USER")
 DB_PASSWORD = env("DB_PASSWORD")
 DB_HOST = env("DB_HOST")
 DB_PORT = env("DB_PORT")
-ALLOWED_HOST = env("ALLOWED_HOSTS")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOST
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
